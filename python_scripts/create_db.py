c827bfe535642b9b1853eb146adfb8e13837cafa
JMason88 <30500349+JMason88@users.noreply.github.com>
2019-01-08 23:17:49 -0300

    .

--- python_scripts/create_db.py
+++ python_scripts/create_db.py
@@ -4,5 +4,5 @@ conn = psycopg2.connect("dbname=postgres host=localhost port=5432 user=postgres"
 conn.autocommit = True
 cur = conn.cursor()
 
-cur.execute("CREATE DATABASE storm_data")
+cur.execute('CREATE DATABASE storm_data')
 conn.commit()
